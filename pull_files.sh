cabaa3e2f98db0e026ed7bc7b84762e7ccc78e16
MxFxM <mxfxmmueller@gmail.com>
2020-12-10 00:07:40 +0100

    scripts to push and pull the files between their directories and this repo. used because linking did not work as expected

---
+++ pull_files.sh
@@ -0,0 +1,8 @@
+#
+
+# run this script to pull all the files from the directories to this repository
+
+sudo cp ~/.bashrc ./bashconfig
+sudo cp ~/.config/qtile/scripts/autostart.sh ./qtile_autostart
+sudo cp ~/.config/qtile/config.py ./qtileconfig
+sudo cp ~/.config/sxhkd/sxhkdrc ./sxhkdconfig
---
+++ push_files.sh
@@ -0,0 +1,8 @@
+#
+
+# run this script to push all the files from this repository to the directories
+
+sudo cp ./bashconfig ~/.bashrc
+sudo cp ./qtile_autostart ~/.config/qtile/scripts/autostart.sh
+sudo cp ./qtileconfig ~/.config/qtile/config.py
+sudo cp ./sxhkdconfig ~/.config/sxhkd/sxhkdrc
